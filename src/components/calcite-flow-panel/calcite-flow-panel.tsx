4e2d86232100a5528cbb99f1ffc447221fc6d866
Matt Driscoll <MDriscoll@esri.com>
2019-06-10 11:42:29 -0700

    flowmanager updates

--- src/components/calcite-flow-panel/calcite-flow-panel.tsx
+++ src/components/calcite-flow-panel/calcite-flow-panel.tsx
@@ -1,4 +1,37 @@
-import { Component, Element, h, Host, Prop } from "@stencil/core";
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  h,
+  Host,
+  Prop
+} from "@stencil/core";
+
+import { chevronLeft16F, ellipsis16F } from "@esri/calcite-ui-icons";
+
+const CSS = {
+  frame: "calcite-flow-panel__frame",
+  header: "calcite-flow-panel__header",
+  headingContainer: "calcite-flow-panel__heading-container",
+  backButton: "calcite-flow-panel__back-button",
+  menuButton: "calcite-flow-panel__menu-button",
+  menu: "calcite-flow-panel__menu",
+  contentContainer: "calcite-flow-panel__content-container",
+  footer: "calcite-flow-panel__footer"
+};
+
+interface Labels {
+  openMenu: string;
+  closeMenu: string;
+  back: string;
+}
+
+const DEFAULT_LABELS = {
+  back: "Back",
+  openMenu: "Open menu",
+  closeMenu: "Close menu"
+};
 
 @Component({
   tag: "calcite-flow-panel",
@@ -25,10 +58,10 @@ export class CalciteFlowPanel {
   @Prop({ reflect: true }) backButton = false;
 
   //----------------------------------
-  //  label
+  //  labels
   //----------------------------------
 
-  @Prop({ reflect: true }) label: string = null;
+  @Prop() labels: Labels = DEFAULT_LABELS;
 
   //----------------------------------
   //  heading
@@ -36,11 +69,19 @@ export class CalciteFlowPanel {
 
   @Prop({ reflect: true }) heading: string = null;
 
+  //----------------------------------
+  //  menuOpen
+  //----------------------------------
+
+  @Prop({ reflect: true }) menuOpen = false;
+
   //--------------------------------------------------------------------------
   //
   //  Events
   //
   //--------------------------------------------------------------------------
+
+  @Event() calciteFlowPanelBackClick: EventEmitter;
 
   //--------------------------------------------------------------------------
   //
@@ -48,51 +89,79 @@ export class CalciteFlowPanel {
   //
   //--------------------------------------------------------------------------
 
-  // frame
-  //   header -
-  //     back button (appears if more than one flow)
-  //     title
-  //     menu button (appears if menu exists)
-  //       list of actions
-  //   content
-  //   footer - just takes actions inside
-
   render() {
-    const { label, backButton, el, heading } = this;
+    const { backButton, el, heading, menuOpen, labels } = this;
 
-    const labelFallback = label || heading;
-
-    const backButtonNode = backButton ? <button>back</button> : null;
+    const backButtonNode = backButton ? (
+      <button
+        key="back-button"
+        class={CSS.backButton}
+        aria-label={labels.back}
+        title={labels.back}
+        onClick={this._backButtonClick.bind(this)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="16"
+          width="16"
+          viewBox="0 0 16 16"
+        >
+          <path d={chevronLeft16F} />
+        </svg>
+      </button>
+    ) : null;
 
     const hasMenuActions = !!el.querySelector("[slot=menu-actions]");
     const hasFooterActions = !!el.querySelector("[slot=footer-actions]");
 
-    console.log(hasMenuActions);
+    const menuLabel = menuOpen ? labels.closeMenu : labels.openMenu;
 
-    const menuActionsSlot = <slot name="menu-actions" />;
-
-    const menuButtonNode = hasMenuActions ? <button>menu</button> : null;
-
-    const menuActionsContainerNode = hasMenuActions ? (
-      <div>{menuActionsSlot}</div>
+    const menuButtonNode = hasMenuActions ? (
+      <button
+        key="menu-button"
+        class={CSS.menuButton}
+        aria-label={menuLabel}
+        title={menuLabel}
+        onClick={this._toggleMenuOpen.bind(this)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="16"
+          width="16"
+          viewBox="0 0 16 16"
+        >
+          <path d={ellipsis16F} />
+        </svg>
+      </button>
     ) : null;
 
-    const footerActionsSlot = <slot name="footer-actions" />;
+    const menuActionsContainerNode =
+      hasMenuActions && menuOpen ? (
+        <div key="menu-actions" class={CSS.menu}>
+          <slot name="menu-actions" />
+        </div>
+      ) : null;
 
     const footerActionsContainerNode = hasFooterActions ? (
-      <footer>{footerActionsSlot}</footer>
+      <footer class={CSS.footer}>
+        <slot name="footer-actions" />
+      </footer>
     ) : null;
 
     const contentContainerNode = (
-      <section>
+      <section class={CSS.contentContainer}>
         <slot />
       </section>
     );
 
+    const headingContainerNode = (
+      <span class={CSS.headingContainer}>{heading}</span>
+    );
+
     const headerNode = (
-      <header aria-label={labelFallback} title={labelFallback}>
+      <header class={CSS.header}>
         {backButtonNode}
-        {heading}
+        {headingContainerNode}
         {menuButtonNode}
         {menuActionsContainerNode}
       </header>
@@ -100,12 +169,26 @@ export class CalciteFlowPanel {
 
     return (
       <Host>
-        <article>
+        <article class={CSS.frame}>
           {headerNode}
           {contentContainerNode}
           {footerActionsContainerNode}
         </article>
       </Host>
     );
+  }
+
+  //--------------------------------------------------------------------------
+  //
+  //  Private Methods
+  //
+  //--------------------------------------------------------------------------
+
+  private _toggleMenuOpen(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  private _backButtonClick(): void {
+    this.calciteFlowPanelBackClick.emit(this);
   }
 }
