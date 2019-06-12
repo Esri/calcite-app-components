92aba8acc3f991739b627673b020ab9ee12095f7
Harry Robbins <hrobbins@esri.com>
2019-06-12 13:50:54 -0700

    change id to storageId

--- src/components/calcite-tip/calcite-tip.e2e.ts
+++ src/components/calcite-tip/calcite-tip.e2e.ts
@@ -1,42 +1,56 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage } from "@stencil/core/testing";
 
-describe('calcite-tip', () => {
-  it('should render', async () => {
+describe("calcite-tip", () => {
+  it("should render", async () => {
     const page = await newE2EPage();
-    await page.setContent(`<calcite-tip><p>basic render</p></calcite-tip>`).catch(error =>{
-      console.error(error);
-    });
-    const el = await page.find('calcite-tip');
+
+    await page
+      .setContent(`<calcite-tip><p>basic render</p></calcite-tip>`)
+      .catch(error => {
+        console.error(error);
+      });
+
+    const el = await page.find("calcite-tip");
     expect(el).not.toBeNull();
   });
 
-  it('should be hidden after the close button is clicked', async () => {
+  it("should be hidden after the close button is clicked", async () => {
     const page = await newE2EPage();
-    await page.setContent(`<calcite-tip><p>testing close button</p></calcite-tip>`).catch(error =>{
-      console.error(error);
-    });
+    await page
+      .setContent(`<calcite-tip><p>testing close button</p></calcite-tip>`)
+      .catch(error => {
+        console.error(error);
+      });
     const tip = await page.find("calcite-tip");
-    const closeButton = await page.find('calcite-tip >>> .close');
+    const closeButton = await page.find("calcite-tip >>> .close");
 
     closeButton.click();
     await page.waitForChanges();
     const isVisible = await tip.isVisible();
     expect(isVisible).toBe(false);
   });
-  it('should hide by default if tip with an id is dismissed', async () => {
+  it("should hide by default if tip with an id is dismissed", async () => {
     const page = await newE2EPage();
-    await page.setContent(`<calcite-tip id="foo"><p>testing localstorage</p></calcite-tip>`).catch(error =>{
-      console.error(error);
-    });
+    await page
+      .setContent(
+        `<calcite-tip storageId="foo"><p>testing localstorage</p></calcite-tip>`
+      )
+      .catch(error => {
+        console.error(error);
+      });
 
-    const closeButton = await page.find('calcite-tip >>> .close');
+    const closeButton = await page.find("calcite-tip >>> .close");
     closeButton.click();
     await page.waitForChanges();
 
     const page2 = await newE2EPage();
-    await page2.setContent(`<calcite-tip id="foo"><p>testing localstorage</p></calcite-tip>`).catch(error =>{
-      console.error(error);
-    });
+    await page2
+      .setContent(
+        `<calcite-tip storageId="foo"><p>testing localstorage</p></calcite-tip>`
+      )
+      .catch(error => {
+        console.error(error);
+      });
 
     const tip = await page2.find("calcite-tip");
     const isVisible = await tip.isVisible();
--- src/components/calcite-tip/calcite-tip.tsx
+++ src/components/calcite-tip/calcite-tip.tsx
@@ -8,45 +8,43 @@ import localStorage from "../../utils/localStorage";
   shadow: true
 })
 export class CalciteTip {
-
   @Element() el: HTMLElement;
 
-  @Prop() id:string = "";
-  @Prop() header:string = "";
-  @Prop() thumbnail:string = "";
-
-  dismissed = false;
+  @Prop() storageId = "";
+  @Prop() dismissed = false;
 
   //@ts-ignore
   closeTip(event: Event) {
     this.el.setAttribute("hidden", "");
-    if(this.id) {
-      localStorage.setItem(this.id, "dismissed");
+    if (this.storageId) {
+      localStorage.setItem(this.storageId, "dismissed");
     }
   }
 
   constructor() {
-    const storageVal = localStorage.getItem(this.id);
+    const storageVal = localStorage.getItem(this.storageId);
     this.dismissed = storageVal !== null;
   }
 
   render() {
     return (
       <Host hidden={this.dismissed}>
-        <slot name="header" />
+        <slot name="heading" />
         <div class="close" onClick={event => this.closeTip(event)}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d={x24}/>
+            <path d={x24} />
           </svg>
         </div>
         <div class="content">
-            <slot name="thumbnail" />
-            <div>
-              <slot />
-              { !!this.el.querySelector('[slot=link]') ? <p class="link">
+          <slot name="thumbnail" />
+          <div>
+            <slot />
+            {!!this.el.querySelector("[slot=link]") ? (
+              <p class="link">
                 <slot name="link" />
-              </p> : null }
-              </div>
+              </p>
+            ) : null}
+          </div>
         </div>
       </Host>
     );
