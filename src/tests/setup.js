1e6016737dac121d89b069eeb89978841f825295
Harry Robbins <hrobbins@esri.com>
2019-06-12 13:51:42 -0700

    Added a reference to github issue tracking workaround

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -4,6 +4,7 @@
 ** Source: <projectRoot>\node_modules\@stencil\core\testing\jest-setuptestframework.js
 ** Reference: https://github.com/ionic-team/stencil/blob/master/src/testing/jest/jest-setup-test-framework.ts
 **            https://github.com/ionic-team/stencil/blob/master/src/testing/index.ts#L6
+** Open Issue: https://github.com/ionic-team/stencil/issues/1634
 */
 
 const { jestSetupTestFramework } = require('@stencil/core/dist/testing');
