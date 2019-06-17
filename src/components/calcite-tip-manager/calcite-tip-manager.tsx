96233bb8c88622a3afc0f58cc7f2a3b8af00c17f
Harry Robbins <hrobbins@esri.com>
2019-06-17 12:20:56 -0700

    finish functionality for tip manager

--- src/components/calcite-tip-manager/calcite-tip-manager.tsx
+++ src/components/calcite-tip-manager/calcite-tip-manager.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Method, Prop, State, Element } from "@stencil/core";
+import { Component, Host, h, Method, State, Element, Watch } from "@stencil/core";
 import { x24, chevronLeft24, chevronRight24 } from "@esri/calcite-ui-icons";
 
 @Component({
@@ -12,20 +12,39 @@ export class CalciteTipManager {
 
   // Lifecycle
 
-  constructor() {
+  connectedCallback() {
+    this.tips = this.el.querySelectorAll('calcite-tip');
     this.total = this.el.querySelectorAll('calcite-tip').length;
-    this.selectedIndex = this.selectedTipIndex;
+    this.tips.forEach((item, index) => {
+      item.setAttribute("dismissible", "false");
+      if(item.hasAttribute("selected")) {
+        this.selectedIndex = index;
+      }
+    });
+    this.updateSelectedTip();
   }
 
   // Props
 
-  @Prop({ reflect: true }) selectedTipIndex = 0;
-
   // State
 
-  @State() dismissed = true;
-  @State() selectedIndex = -1;
+  @State() dismissed = false;
+  @State() selectedIndex = 0;
+  DEFAULT_GROUP_TITLE = "Tips"; //TODO: localize this text?
+  groupTitle = '';
   total = 0;
+  tips = null;
+  title = null;
+  tipContainer = null;
+  direction: "forward" | "backward" = "forward";
+
+  // Watchers
+
+  @Watch('selectedIndex')
+  selectedChangeHandler() {
+    this.triggerAnimation();
+    this.updateSelectedTip();
+  }
 
   // Methods
 
@@ -39,38 +58,61 @@ export class CalciteTipManager {
   }
 
   nextTip() {
-
+    this.direction = "forward";
+    this.selectedIndex++;
+    if(this.selectedIndex > this.total-1) {
+      this.selectedIndex = 0;
+    }
   }
-  prevTip() {
+  previousTip() {
+    this.direction = "backward";
+    this.selectedIndex--;
+    if(this.selectedIndex < 0) {
+      this.selectedIndex = this.total-1;
+    }
+  }
 
+  triggerAnimation() {
+    this.tipContainer.classList.remove("is-animating", "forward", "backward");
+    void this.tipContainer.offsetWidth; // hack for restarting animation. https://css-tricks.com/restart-css-animation/
+    this.tipContainer.classList.add('is-animating', this.direction);
+  }
+
+  updateSelectedTip() {
+    this.tips.forEach((tip, index) => {
+      if(index === this.selectedIndex) {
+        this.groupTitle = tip.dataset.groupTitle ? tip.dataset.groupTitle : this.DEFAULT_GROUP_TITLE;
+        tip.setAttribute("selected", "true");
+      } else {
+        tip.setAttribute("selected", "false");
+      }
+    });
   }
 
   // Render
 
-
-
   render() {
     return (
       <Host hidden={this.dismissed}>
-        <header class="tipManagerHeader">
-          <slot name="title"></slot>
+        <header class="header">
+          <h2 ref={(el) => this.title = el as HTMLInputElement} class="title">{this.groupTitle}</h2>
           <div class="close" onClick={() => this.hideTipManager()}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d={x24} />
             </svg>
           </div>
         </header>
-        <div class="tipManagerContent">
+        <div class="tipContainer" ref={el => this.tipContainer = el as HTMLElement}>
           <slot />
         </div>
-        <footer>
-          <button class="pageLeftControl">
+        <footer class="pagination">
+          <button class="pageControl pageControl--left" onClick={()=> { this.previousTip(); }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d={chevronLeft24} />
             </svg>
           </button>
-          <span class="pagePosition">{`Tip ${this.selectedIndex}/${this.total}`}</span>
-          <button class="pageRightControl">
+          <span class="pagePosition">{`Tip ${this.selectedIndex+1}/${this.total}`}</span>
+          <button class="pageControl pageControl--right" onClick={()=> { this.nextTip(); }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d={chevronRight24} />
             </svg>
