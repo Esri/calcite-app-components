4d08a83011778eec88348ac0170f3be30d5e3731
JC Franco <jfranco@esri.com>
2019-06-18 10:51:11 -0700

    Issue #23: Use latest plugin types.

--- stencil.config.ts
+++ stencil.config.ts
@@ -30,8 +30,7 @@ export const config: Config = {
       injectGlobalPaths: ["src/assets/styles/includes.scss"]
     }),
 
-    // drop any type when https://github.com/ionic-team/stencil-postcss/pull/16 lands
-    (postcss as any)({
+    postcss({
       plugins: [autoprefixer()]
     })
   ]
