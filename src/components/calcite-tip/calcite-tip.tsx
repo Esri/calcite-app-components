76ac2f1df22425d7681e7c50b4be0ce10d71e156
Harry Robbins <hrobbins@esri.com>
2019-06-18 11:50:54 -0700

    adjusted ordering per discussion and ran prettier

--- src/components/calcite-tip/calcite-tip.tsx
+++ src/components/calcite-tip/calcite-tip.tsx
@@ -10,7 +10,7 @@ import { getItem, setItem } from "../../utils/localStorage";
 export class CalciteTip {
   @Element() el: HTMLElement;
 
-  @Prop() storageId:string;
+  @Prop() storageId: string;
   @Prop() dismissible = true;
 
   @State() dismissed = getItem(`calcite-tip-${this.storageId}`) !== null;
@@ -26,11 +26,13 @@ export class CalciteTip {
     return (
       <Host hidden={this.dismissed}>
         <slot name="heading" />
-        { this.dismissible ? <div class="close" onClick={() => this.hideTip()}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d={x24} />
-          </svg>
-        </div> : null }
+        {this.dismissible ? (
+          <div class="close" onClick={() => this.hideTip()}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d={x24} />
+            </svg>
+          </div>
+        ) : null}
         <div class="content">
           <slot name="thumbnail" />
           <div>
