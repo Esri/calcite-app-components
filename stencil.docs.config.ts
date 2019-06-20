7ee4700f0a3fac4dff6a8ab0aaae2e11005978e6
Harry Robbins <hrobbins@esri.com>
2019-06-20 11:49:10 -0700

    add a new script and config for generating docs

---
+++ stencil.docs.config.ts
@@ -0,0 +1,26 @@
+import { Config } from "@stencil/core";
+import { postcss } from "@stencil/postcss";
+import { sass } from "@stencil/sass";
+import autoprefixer from "autoprefixer";
+
+export const config: Config = {
+  namespace: "calcite",
+  outputTargets: [
+    {
+      type: "www",
+      dir: "docs",
+      copy: [{ src: "demos" }],
+      serviceWorker: null // disable service workers
+    }
+  ],
+  globalStyle: "src/assets/styles/includes.scss",
+  plugins: [
+    sass({
+      injectGlobalPaths: ["src/assets/styles/includes.scss"]
+    }),
+
+    postcss({
+      plugins: [autoprefixer()]
+    })
+  ]
+};
