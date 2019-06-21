045936930c643fd8b537c4f4a22a5963957f1cae
Matt Driscoll <mdriscoll@esri.com>
2019-06-21 12:04:46 -0700

    test

--- src/components/calcite-action-bar/calcite-action-bar.e2e.ts
+++ src/components/calcite-action-bar/calcite-action-bar.e2e.ts
@@ -1,6 +1,7 @@
 import { newE2EPage } from "@stencil/core/testing";
+import { CalciteActionBar } from "./calcite-action-bar";
 
-describe.skip("calcite-action-bar", () => {
+describe("calcite-action-bar", () => {
   it("renders", async () => {
     const page = await newE2EPage();
 
@@ -8,4 +9,65 @@ describe.skip("calcite-action-bar", () => {
     const element = await page.find("calcite-action-bar");
     expect(element).toHaveClass("hydrated");
   });
+
+  it("defaults", async () => {
+    const bar = new CalciteActionBar();
+
+    expect(bar.expand).toBe(true);
+    expect(bar.expanded).toBe(false);
+    expect(bar.labels.expand).toEqual("Expand");
+    expect(bar.labels.collapse).toEqual("Collapse");
+  });
+
+  // it("Default label", async () => {
+  //   const page = await newE2EPage();
+
+  //   await page.setContent("<calcite-action-bar expand></calcite-action-bar>");
+
+  //   await page.waitForChanges();
+
+  //   const expandAction = await page.find(
+  //     "calcite-action-bar >>> calcite-action"
+  //   );
+
+  //   expect(expandAction).not.toBeNull();
+  //   expect(expandAction.getProperty("text")).toBe("Expand");
+  // });
+
+  // it("expand", async () => {
+  //   const page = await newE2EPage();
+
+  //   await page.setContent("<calcite-action-bar expand></calcite-action-bar>");
+
+  //   await page.waitForChanges();
+
+  //   const expandAction = await page.find(
+  //     "calcite-action-bar >>> calcite-action"
+  //   );
+
+  //   expect(expandAction).not.toBeNull();
+  // });
+
+  // it("expanded", async () => {
+  //   const page = await newE2EPage();
+
+  //   await page.setContent("<calcite-action-bar expand></calcite-action-bar>");
+
+  //   //const eventSpy = await page.spyOnEvent("calciteActionClick", "window");
+
+  //   const bar = await page.find("calcite-action-bar");
+
+  //   const button = await page.find(
+  //     "calcite-action-bar >>> calcite-action >>> .button"
+  //   );
+
+  //   expect(button).not.toBeNull();
+
+  //   button.click();
+
+  //   await page.waitForChanges();
+
+  //   //expect(eventSpy).toHaveReceivedEvent();
+  //   expect(bar.getProperty("expanded")).toBe(true);
+  // });
 });
