b957df40de7ae65aa63bd1042436bdeec62a6db3
Matt Driscoll <MDriscoll@esri.com>
2019-06-24 09:12:16 -0700

    test progress

--- src/components/calcite-flow/calcite-flow.e2e.ts
+++ src/components/calcite-flow/calcite-flow.e2e.ts
@@ -1,11 +1,25 @@
 import { newE2EPage } from "@stencil/core/testing";
 
-describe("my-component", () => {
+describe("calcite-flow", () => {
   it("renders", async () => {
     const page = await newE2EPage();
 
-    await page.setContent("<my-component></my-component>");
-    const element = await page.find("my-component");
+    await page.setContent("<calcite-flow></calcite-flow>");
+    const element = await page.find("calcite-flow");
     expect(element).toHaveClass("hydrated");
   });
+
+  it("has frame", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent("<calcite-flow></calcite-flow>");
+    const element = await page.find("calcite-flow");
+    expect(element).toHaveClass("frame");
+  });
+
+  // it("frame advancing", async () => {});
+
+  // it("frame retreating", async () => {});
+
+  // it("back()", async () => {});
 });
