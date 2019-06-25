41851674b296254f10f28fa807092bad553d56e3
Matt Driscoll <MDriscoll@esri.com>
2019-06-25 14:02:56 -0700

    position tests

--- src/components/calcite-floating-panel/calcite-floating-panel.e2e.ts
+++ src/components/calcite-floating-panel/calcite-floating-panel.e2e.ts
@@ -1,20 +1,20 @@
 import { newE2EPage } from "@stencil/core/testing";
 
-describe("calcite-action-pad", () => {
+describe("calcite-floating-panel", () => {
   it("renders", async () => {
     const page = await newE2EPage();
 
-    await page.setContent("<calcite-action-pad></calcite-action-pad>");
-    const element = await page.find("calcite-action-pad");
+    await page.setContent("<calcite-floating-panel></calcite-floating-panel>");
+    const element = await page.find("calcite-floating-panel");
     expect(element).toHaveClass("hydrated");
   });
 
   it("position defaults", async () => {
     const page = await newE2EPage();
 
-    await page.setContent("<calcite-action-pad></calcite-action-pad>");
+    await page.setContent("<calcite-floating-panel></calcite-floating-panel>");
 
-    const element = await page.find("calcite-action-pad");
+    const element = await page.find("calcite-floating-panel");
     const positionType = await element.getProperty("positionType");
     const positionElement = await element.getProperty("positionElement");
 
