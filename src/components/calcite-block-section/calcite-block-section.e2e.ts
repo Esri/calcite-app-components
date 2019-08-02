05e5cfaf36e9b74ab31a701afb3f446aed412382
JC Franco <jfranco@esri.com>
2019-08-02 11:37:01 -0700

    Make block tests similar.

--- src/components/calcite-block-section/calcite-block-section.e2e.ts
+++ src/components/calcite-block-section/calcite-block-section.e2e.ts
@@ -28,31 +28,51 @@ describe("calcite-block-section", () => {
     expect(element).toBeNull();
   });
 
+  it("can display/hide content", async () => {
+    const page = await newE2EPage();
+    await page.setContent("<calcite-block-section><div>some content</div></calcite-block-section>");
+    let element = await page.find("calcite-block-section");
+    let content = await page.find(`calcite-block-section >>> .${CSS.content}`);
+
+    expect(await content.isVisible()).toBe(false);
+
+    element.setProperty("open", true);
+    await page.waitForChanges();
+    element = await page.find("calcite-block-section[open]");
+    content = await page.find(`calcite-block-section >>> .${CSS.content}`);
+
+    expect(element).toBeTruthy();
+    expect(await content.isVisible()).toBe(true);
+
+    element.setProperty("open", false);
+    await page.waitForChanges();
+    element = await page.find("calcite-block-section[open]");
+    content = await page.find(`calcite-block-section >>> .${CSS.content}`);
+
+    expect(element).toBeNull();
+    expect(await content.isVisible()).toBe(false);
+  });
+
   it("can be toggled", async () => {
     const page = await newE2EPage();
     await page.setContent("<calcite-block-section></calcite-block-section>");
     const element = await page.find("calcite-block-section");
-    const content = await page.find(`calcite-block-section >>> .${CSS.content}`);
     const toggleSpy = await element.spyOnEvent("calciteBlockSectionToggle");
     const toggle = await page.find(`calcite-block-section >>> calcite-action`);
 
-    expect(await element.getProperty("open")).toBe(false);
-    expect(await content.isVisible()).toBe(false);
     expect(toggle.getAttribute("aria-label")).toBe(TEXT.expand);
 
     await toggle.click();
 
-    expect(await element.getProperty("open")).toBe(true);
-    expect(await content.isVisible()).toBe(true);
-    expect(toggle.getAttribute("aria-label")).toBe(TEXT.collapse);
     expect(toggleSpy).toHaveReceivedEventTimes(1);
+    expect(await element.getProperty("open")).toBe(true);
+    expect(toggle.getAttribute("aria-label")).toBe(TEXT.collapse);
 
     await toggle.click();
 
-    expect(await element.getProperty("open")).toBe(false);
-    expect(await content.isVisible()).toBe(false);
-    expect(toggle.getAttribute("aria-label")).toBe(TEXT.expand);
     expect(toggleSpy).toHaveReceivedEventTimes(2);
+    expect(await element.getProperty("open")).toBe(false);
+    expect(toggle.getAttribute("aria-label")).toBe(TEXT.expand);
   });
 
   it("sets calcite-block-section renders section text", async () => {
