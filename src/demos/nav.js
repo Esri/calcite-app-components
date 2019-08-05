f310c484c1059baa3cf1236bc84baeb7986c261a
Matt Driscoll <mdriscoll@esri.com>
2019-08-05 10:05:17 -0700

    get nav working in edge

--- src/demos/nav.js
+++ src/demos/nav.js
@@ -1,78 +1,103 @@
-const root = window.location.pathname.split('demos').shift();
+var calciteNavItems = [
+  {
+    name: `<!-- taken from https://esri.github.io/calcite-ui-icons/#home -->
+    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 16 16">
+      <title>Home</title>
+      <path fill="#fff" d="M16 8h-1v8h-5v-5H7v5H2V8H1l2-2.133V0h3v2.667L8.5 0z"/>
+    </svg>`,
+    path: "/",
+  },
+  {
+    name: "Action",
+    path: "/demos/calcite-action.html"
+  },
+  {
+    name: "Action Bar",
+    path: "/demos/calcite-action-bar.html"
+  },
+  {
+    name: "Action Pad",
+    path: "/demos/calcite-action-pad.html"
+  },
+  {
+    name: "Block",
+    path: "/demos/calcite-block.html"
+  },
+  {
+    name: "Flow",
+    path: "/demos/calcite-flow.html"
+  },
+  {
+    name: "Flow Item",
+    path: "/demos/calcite-flow-item.html"
+  },
+  {
+    name: "Shell",
+    path: "/demos/calcite-shell.html"
+  },
+  {
+    name: "Shell Panel",
+    path: "/demos/calcite-shell-panel.html"
+  },
+  {
+    name: "Tip",
+    path: "/demos/calcite-tip.html"
+  },
+  {
+    name: "Tip Manager",
+    path: "/demos/calcite-tip-manager.html"
+  },
+  {
+    name: "Typography",
+    path: "/demos/calcite-typography.html"
+  }
+];
 
-const template = document.createElement('template');
-template.innerHTML = `
+var calciteNavContainer = document.createElement('div');
+calciteNavContainer.innerHTML = `
 <style>
-nav {
+.styleguide-nav {
   width: 100vw;
   background-color: #333;
   color: #FFF;
-}
-ul {
   display: flex;
   list-style: none;
   margin: 0;
   padding: 0;
 }
-li {
+.styleguide-nav li {
   display: flex;
   border-left: 1px solid white;
 }
-li:first-child {
+.styleguide-nav li:first-child {
   border-left: none;
 }
-li:last-child {
+.styleguide-nav li:last-child {
   border-right: 1px solid white;
 }
-a {
+.styleguide-nav a {
   display: flex;
   align-items: center;
   padding: 12px 24px;
   color: inherit;
   text-decoration: none;
 }
-a:hover {
+.styleguide-nav a:hover {
   background-color: black;
 }
-a.is-active {
+.styleguide-nav a.is-active {
   background-color: #008DE4;
 }
 </style>
-<nav class="styleguide--nav">
-    <ul>
-      <li>
-      <a href="${root}">
-        <!-- taken from https://esri.github.io/calcite-ui-icons/#home -->
-        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 16 16">
-          <title>Home</title>
-          <path fill="#fff" d="M16 8h-1v8h-5v-5H7v5H2V8H1l2-2.133V0h3v2.667L8.5 0z"/>
-        </svg>
-      </a>
-      </li>
-      <li><a href="${root}demos/calcite-action.html">Action</a></li>
-      <li><a href="${root}demos/calcite-action-bar.html">Action Bar</a></li>
-      <li><a href="${root}demos/calcite-action-pad.html">Action Pad</a></li>
-      <li><a href="${root}demos/calcite-block.html">Block</a></li>
-      <li><a href="${root}demos/calcite-flow.html">Flow</a></li>
-      <li><a href="${root}demos/calcite-flow-item.html">Flow Item</a></li>
-      <li><a href="${root}demos/calcite-shell.html">Shell</a></li>
-      <li><a href="${root}demos/calcite-shell-panel.html">Shell Panel</a></li>
-      <li><a href="${root}demos/calcite-tip.html">Tip</a></li>
-      <li><a href="${root}demos/calcite-tip-manager.html">Tip Manager</a></li>
-      <li><a href="${root}demos/calcite-typography.html">Typography</a></li>
-    </ul>
-</nav>
+
+<ul class="styleguide-nav">
+  ${calciteNavItems.map(item => {
+    return `<li><a class="${window.location.pathname === item.path ? "is-active" : ""}" href="${item.path}">${item.name}</a></li>`;
+  }).join("")}
+</ul>
 `;
 
-class StyleguideNav extends HTMLElement {
-  constructor() {
-    super();
-    this._shadowRoot = this.attachShadow({ 'mode': 'open' });
-    this._shadowRoot.appendChild(template.content.cloneNode(true));
-  }
-  connectedCallback() {
-    const activeLink = this._shadowRoot.querySelector(`a[href*="${window.location.pathname}"]`);
-    activeLink && activeLink.classList.add('is-active');
-  }
+window.onload = function(){
+  window.document.body.prepend(calciteNavContainer);
 }
-window.customElements.define('calcite-styleguide-nav', StyleguideNav);
+
