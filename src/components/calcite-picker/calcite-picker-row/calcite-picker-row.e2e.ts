c5980b70a442761bed818c023b300b91badf058b
Harry Robbins <hrobbins@esri.com>
2019-08-15 16:49:17 -0700

    add test for click behavior

--- src/components/calcite-picker/calcite-picker-row/calcite-picker-row.e2e.ts
+++ src/components/calcite-picker/calcite-picker-row/calcite-picker-row.e2e.ts
@@ -11,4 +11,14 @@ describe("calcite-picker-row", () => {
     const isVisible = await row.isVisible();
     expect(isVisible).toBe(true);
   });
+
+  it("should toggle selected attribute when clicked", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<calcite-picker-row></calcite-picker-row>`);
+    const row = await page.find("calcite-picker-row");
+    expect(row.selected).toBe(false);
+    await row.click();
+    expect(row.selected).toBe(true);
+  });
 });
