3d0cec2201a3152b75111e6444d1dcc64615a394
Matt Driscoll <MDriscoll@esri.com>
2019-08-21 13:13:24 -0700

    remove unused classes

--- src/components/calcite-flow-item/resources.ts
+++ src/components/calcite-flow-item/resources.ts
@@ -4,8 +4,6 @@ export const CSS = {
   heading: "heading",
   headingFirst: "heading--first",
   backButton: "back-button",
-  chevronLeft: "chevron-left",
-  chevronRight: "chevron-right",
   singleActionContainer: "single-action-container",
   menuContainer: "menu-container",
   menuButton: "menu-button",
