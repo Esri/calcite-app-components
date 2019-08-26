a7188a83c12f58b5d833c8d2c7a2219b1e744de2
Matt Driscoll <MDriscoll@esri.com>
2019-08-26 13:54:08 -0700

    change lifecycle method

--- src/components/calcite-action-bar/calcite-action-bar.tsx
+++ src/components/calcite-action-bar/calcite-action-bar.tsx
@@ -69,7 +69,7 @@ export class CalciteActionBar {
   //
   // --------------------------------------------------------------------------
 
-  connectedCallback() {
+  componentWillLoad() {
     this.setTextEnabled(this.expanded);
   }
 
