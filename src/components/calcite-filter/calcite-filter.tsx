8f305249f8bb36620dc4cc98a1de5bde3d4f5a81
Harry Robbins <hrobbins@esri.com>
2019-09-23 09:50:45 -0700

    fix typo

--- src/components/calcite-filter/calcite-filter.tsx
+++ src/components/calcite-filter/calcite-filter.tsx
@@ -27,7 +27,7 @@ export class CalciteFilter {
   //
   // --------------------------------------------------------------------------
 
-  @Element() el: HTMLCacliteFilterElement;
+  @Element() el: HTMLCalciteFilterElement;
 
   // --------------------------------------------------------------------------
   //
