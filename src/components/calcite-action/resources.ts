67ff1fc3b179892f465b6cb49afb7dec31827c5c
Matt Driscoll <MDriscoll@esri.com>
2019-11-04 14:11:40 -0800

    fix action loading in edge.

--- src/components/calcite-action/resources.ts
+++ src/components/calcite-action/resources.ts
@@ -4,5 +4,7 @@ export const CSS = {
   buttonTextInteractive: "button--text-interactive",
   buttonCompact: "button--compact",
   iconContainer: "icon-container",
+  slotContainer: "slot-container",
+  slotContainerHidden: "slot-container--hidden",
   textContainer: "text-container"
 };
