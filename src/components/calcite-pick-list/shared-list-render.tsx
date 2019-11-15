28b76a7081828760950b66347e8ea83e5a78ce44
Matt Driscoll <MDriscoll@esri.com>
2019-11-15 13:49:59 -0800

    revert stuff

--- src/components/calcite-pick-list/shared-list-render.tsx
+++ src/components/calcite-pick-list/shared-list-render.tsx
@@ -3,31 +3,28 @@ import CalciteScrim from "../utils/CalciteScrim";
 import { VNode } from "@esri/calcite-components/dist/types/stencil.core";
 import { CSS } from "./resources";
 
+const renderScrim = (loading, disabled): VNode => {
+  return <CalciteScrim loading={loading} disabled={disabled}></CalciteScrim>;
+};
+
 export const List = ({ props, text, ...rest }): VNode => {
   const { disabled, loading, filterEnabled, dataForFilter, handleFilter } = props;
   const { filterPlaceholder } = text;
-
-  const headerNode = (
-    <header class={{ [CSS.sticky]: true }}>
-      {filterEnabled ? (
-        <calcite-filter
-          data={dataForFilter}
-          textPlaceholder={filterPlaceholder}
-          aria-label={filterPlaceholder}
-          onCalciteFilterChange={handleFilter}
-        />
-      ) : null}
-      <slot name="menu-actions" />
-    </header>
-  );
-
-  const contentNodes = [headerNode, <slot />];
-
   return (
     <Host aria-disabled={disabled} aria-busy={loading} {...rest}>
-      <CalciteScrim loading={loading} disabled={disabled}>
-        {contentNodes}
-      </CalciteScrim>
+      <header class={{ [CSS.sticky]: true }}>
+        {filterEnabled ? (
+          <calcite-filter
+            data={dataForFilter}
+            textPlaceholder={filterPlaceholder}
+            aria-label={filterPlaceholder}
+            onCalciteFilterChange={handleFilter}
+          />
+        ) : null}
+        <slot name="menu-actions" />
+      </header>
+      <slot />
+      {renderScrim(loading, disabled)}
     </Host>
   );
 };
