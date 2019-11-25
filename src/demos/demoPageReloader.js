908bb4f2ce37d61fd0086f386ca96a157e194311
Matt Driscoll <MDriscoll@esri.com>
2019-11-25 15:04:57 -0800

    review fix cleanup

--- src/demos/demoPageReloader.js
+++ src/demos/demoPageReloader.js
@@ -1,11 +1,11 @@
-(function () {
+(function() {
   // based on pieces of https://github.com/ionic-team/stencil/blob/4960fbe11444a18def3ce48ab46b018c416f9472/src/dev-server/dev-client/client-web-socket.ts
   const getSocketUrl = function getSocketUrl(location) {
-    return location.protocol === "https:" ? "wss:" : "ws:" + "//" + location.hostname +  ":" + location.port +  "/";
+    return location.protocol === "https:" ? "wss:" : "ws:" + "//" + location.hostname + ":" + location.port + "/";
   };
 
-  const clientWebSocket = 'WebSocket' in window && new window.WebSocket(getSocketUrl(window.location), ["xmpp"]);
-  clientWebSocket && clientWebSocket.addEventListener("message", function (message) {
+  const clientWebSocket = new window.WebSocket(getSocketUrl(window.location), ["xmpp"]);
+  clientWebSocket.addEventListener("message", function(message) {
     if (message.data.indexOf("rebuild finished") > -1) {
       window.location.reload();
     }
