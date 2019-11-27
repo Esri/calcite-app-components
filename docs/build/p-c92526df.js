20c64a7e0db69b2f22b0fee06dffb9a96216523f
JC Franco <jfranco@esri.com>
2019-11-26 17:38:09 -0800

    4.8.0

---
+++ docs/build/p-c92526df.js
@@ -0,0 +1,1 @@
+const t={action:"action",description:"description",handle:"handle",handleActivated:"handle--activated",highlight:"highlight",icon:"icon",label:"label",title:"title",textContainer:"text-container"};export{t as C};
