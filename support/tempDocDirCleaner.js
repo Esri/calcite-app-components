3d15e2cc4d2713e490ad4bdae5b78dbad35e8cdd
JC Franco <jfranco@esri.com>
2019-12-12 11:24:49 -0800

    chore(docs): ensure storybook doc build runs fully.

--- support/tempDocDirCleaner.js
+++ support/tempDocDirCleaner.js
@@ -19,6 +19,10 @@ process.on("exit", exitHandler.bind(null, { cleanup: true }));
 // catches ctrl+c event
 process.on("SIGINT", exitHandler.bind(null, { exit: true }));
 
+// catches other kill process signals (e.g., concurrently --kill-others ...)
+process.on("SIGHUP", exitHandler.bind(null, { exit: true }));
+process.on("SIGTERM", exitHandler.bind(null, { exit: true }));
+
 // catches "kill pid" (for example: nodemon restart)
 process.on("SIGUSR1", exitHandler.bind(null, { exit: true }));
 process.on("SIGUSR2", exitHandler.bind(null, { exit: true }));
