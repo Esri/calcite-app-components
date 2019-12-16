625c6b9acb31636e7f02eb4197af3afcc684ab4a
Harry Robbins <arrogantgopher@gmail.com>
2019-12-16 14:20:42 -0800

    tip stories

--- src/components/calcite-tip/calcite-tip.stories.ts
+++ src/components/calcite-tip/calcite-tip.stories.ts
@@ -1,6 +1,8 @@
-import { withKnobs } from "@storybook/addon-knobs";
+import { boolean, select, text, withKnobs } from "@storybook/addon-knobs";
 import { Attributes, createComponentHTML as create, darkBackground, parseReadme } from "../../../.storybook/utils";
 import readme from "./readme.md";
+import { TEXT } from "./resources";
+import { ATTRIBUTES } from "../../../.storybook/resources";
 
 export default {
   title: "calcite-tip",
@@ -11,6 +13,37 @@ export default {
   }
 };
 
-const createAttributes: () => Attributes = () => [];
+const { theme } = ATTRIBUTES;
+
+const createAttributes: () => Attributes = () => [
+  {
+    name: "storage-id",
+    value: text("storageId", "tip-12345")
+  },
+  {
+    name: "non-dismissible",
+    value: boolean("nonDismissible", false)
+  },
+  {
+    name: "heading",
+    value: text("heading", "My Tip")
+  },
+  {
+    name: "text-close",
+    value: text("textClose", TEXT.close)
+  },
+  {
+    name: "text-thumbnail",
+    value: text("textThumbnail", "A placeholder image")
+  },
+  {
+    name: "thumbnail",
+    value: text("thumbnail", "https://placeimg.com/1000/600")
+  },
+  {
+    name: "theme",
+    value: select("theme", theme.values, theme.defaultValue)
+  }
+];
 
 export const basic = () => create("calcite-tip", createAttributes());
