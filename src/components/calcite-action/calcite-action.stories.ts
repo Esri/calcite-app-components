a0100a1a1246aac3fd944e9b9a8eb7965d1c5194
Matt Driscoll <MDriscoll@esri.com>
2019-12-17 09:47:42 -0800

    Action story

--- src/components/calcite-action/calcite-action.stories.ts
+++ src/components/calcite-action/calcite-action.stories.ts
@@ -1,6 +1,10 @@
-import { withKnobs } from "@storybook/addon-knobs";
+import { boolean, select, text, withKnobs } from "@storybook/addon-knobs";
 import { Attributes, createComponentHTML as create, darkBackground, parseReadme } from "../../../.storybook/utils";
 import readme from "./readme.md";
+
+import { ATTRIBUTES } from "../../../.storybook/resources";
+import { APPEARANCE_VALUES } from "./resources";
+import { beaker16 } from "@esri/calcite-ui-icons";
 
 export default {
   title: "calcite-action",
@@ -11,6 +15,63 @@ export default {
   }
 };
 
-const createAttributes: () => Attributes = () => [];
+const { theme } = ATTRIBUTES;
 
-export const basic = () => create("calcite-action", createAttributes());
+const createAttributes: () => Attributes = () => [
+  {
+    name: "appearance",
+    value: select("appearance", APPEARANCE_VALUES, "solid")
+  },
+  {
+    name: "active",
+    value: boolean("active", false)
+  },
+  {
+    name: "compact",
+    value: boolean("compact", false)
+  },
+  {
+    name: "disabled",
+    value: boolean("disabled", false)
+  },
+  {
+    name: "indicator",
+    value: boolean("indicator", false)
+  },
+  {
+    name: "label",
+    value: text("label", "Label")
+  },
+  {
+    name: "loading",
+    value: boolean("loading", false)
+  },
+  {
+    name: "text",
+    value: text("text", "Text")
+  },
+  {
+    name: "text-enabled",
+    value: boolean("text-enabled", false)
+  },
+  {
+    name: "theme",
+    value: select("theme", theme.values, theme.defaultValue)
+  }
+];
+
+const iconSize = 16;
+
+const svgIcon = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height="${iconSize}"
+  width="${iconSize}"
+  fill="currentColor"
+  viewBox="0 0 ${iconSize} ${iconSize}"
+>
+  <path d="${beaker16}" />
+</svg>
+`;
+
+export const basic = () => create("calcite-action", createAttributes(), svgIcon);
