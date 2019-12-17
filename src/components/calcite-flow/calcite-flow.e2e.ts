385a8ca78e253bb22e3d594d10d004c6147aa15c
Matt Driscoll <MDriscoll@esri.com>
2019-12-16 16:47:21 -0800

    simplify test

--- src/components/calcite-flow/calcite-flow.e2e.ts
+++ src/components/calcite-flow/calcite-flow.e2e.ts
@@ -3,10 +3,6 @@ import { newE2EPage } from "@stencil/core/testing";
 import { CSS } from "./resources";
 import { accessible, hidden, renders } from "../../tests/commonTests";
 
-function createTestPromise(success: boolean, timeout = 0) {
-  return new Promise((resolve, reject) => setTimeout(() => (success ? resolve() : reject("error")), timeout));
-}
-
 describe("calcite-flow", () => {
   it("renders", async () => renders("calcite-flow"));
 
@@ -40,51 +36,24 @@ describe("calcite-flow", () => {
     expect(flowItem).toBeNull();
   });
 
-  it("setting beforeBack should wait until resolved before going back", async () => {
+  it("setting 'beforeBack' should be called in 'back()'", async () => {
     const page = await newE2EPage();
+
+    const mockCallBack = jest.fn();
+    await page.exposeFunction("beforeBack", mockCallBack);
 
     await page.setContent("<calcite-flow><calcite-flow-item></calcite-flow-item></calcite-flow>");
 
-    let flowItem = await page.find("calcite-flow-item");
-
-    expect(flowItem).not.toBeNull();
-
-    flowItem.setProperty("beforeBack", () => createTestPromise(true));
+    await page.$eval("calcite-flow-item", (elm: HTMLCalciteFlowItemElement) => {
+      elm.beforeBack = this.beforeBack;
+    });
 
     const flow = await page.find("calcite-flow");
 
     await flow.callMethod("back");
 
-    await page.waitForChanges();
-
-    flowItem = await page.find("calcite-flow-item");
-
-    expect(flowItem).toBeNull();
+    expect(mockCallBack).toBeCalled();
   });
-
-  /*
-  it("setting beforeBack with rejected promise should not remove flow-item", async () => {
-    const page = await newE2EPage();
-
-    await page.setContent("<calcite-flow><calcite-flow-item></calcite-flow-item></calcite-flow>");
-
-    let flowItem = await page.find("calcite-flow-item");
-
-    expect(flowItem).not.toBeNull();
-
-    flowItem.setProperty("beforeBack", () => createTestPromise(false));
-
-    const flow = await page.find("calcite-flow");
-
-    await flow.callMethod("back");
-
-    await page.waitForChanges();
-
-    flowItem = await page.find("calcite-flow-item");
-
-    expect(flowItem).not.toBeNull();
-  });
-  */
 
   it("frame advancing should add animation class", async () => {
     const page = await newE2EPage();
