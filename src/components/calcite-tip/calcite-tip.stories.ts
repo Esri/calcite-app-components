0beefec3c65ae5e793e7f213c3f01e985c7de953
Matt Driscoll <MDriscoll@esri.com>
2019-12-17 14:05:09 -0800

    add slots to tip

--- src/components/calcite-tip/calcite-tip.stories.ts
+++ src/components/calcite-tip/calcite-tip.stories.ts
@@ -46,4 +46,6 @@ const createAttributes: () => Attributes = () => [
   }
 ];
 
-export const basic = () => create("calcite-tip", createAttributes());
+const html = `<div slot="info">Enim nascetur erat faucibus ornare varius arcu fames bibendum habitant felis elit ante. Nibh morbi massa curae; leo semper diam aenean congue taciti eu porta. Varius faucibus ridiculus donec. Montes sit ligula purus porta ante lacus habitasse libero cubilia purus! In quis congue arcu maecenas felis cursus pellentesque nascetur porta donec non. Quisque, rutrum ligula pharetra justo habitasse facilisis rutrum neque. Magnis nostra nec nulla dictumst taciti consectetur. Non porttitor tempor orci dictumst magna porta vitae.</div><a slot="link" href="http://www.esri.com">This is the "link" slot.</a>`;
+
+export const basic = () => create("calcite-tip", createAttributes(), html);
