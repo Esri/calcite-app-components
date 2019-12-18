7d487480f1d73805caf3a9e749a5a66e349174c0
Matt Driscoll <MDriscoll@esri.com>
2019-12-18 11:44:13 -0800

    cleanup

--- .storybook/addons.ts
+++ .storybook/addons.ts
@@ -1,4 +1,4 @@
 // tslint:disable:no-import-side-effect
 import "@storybook/addon-backgrounds/register";
-import "@storybook/addon-notes/register-panel";
 import "@storybook/addon-knobs/register";
+import "@storybook/addon-notes/register-panel";
