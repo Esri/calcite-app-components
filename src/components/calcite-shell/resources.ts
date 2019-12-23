24c8e5b3b7d1b048dad478f79f72c937bddee264
Matt Driscoll <mdriscoll@esri.com>
2019-12-23 10:27:20 -0800

    Enhancement: ShellPanel. Add tests and stories for detached mode and panel heigh-scale prop #686

--- src/components/calcite-shell/resources.ts
+++ src/components/calcite-shell/resources.ts
@@ -1,5 +1,9 @@
+import { CalciteScale } from "../interfaces";
+
 export const CSS = {
   main: "main",
   content: "content",
   footer: "footer"
 };
+
+export const SCALES: CalciteScale[] = ["s", "m", "l"];
