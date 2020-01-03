ad2471b4293944ed151b7c57caef160e8afbd220
Alan Sangma <asangma@esri.com>
2020-01-03 13:48:03 -0800

    reset e2e

--- src/components/calcite-flow-item/calcite-flow-item.e2e.ts
+++ src/components/calcite-flow-item/calcite-flow-item.e2e.ts
@@ -204,7 +204,7 @@ describe("calcite-flow-item", () => {
   it("should be accessible", async () =>
     accessible(`
       <calcite-flow-item heading="hello world" summary="test" menu-open show-back-button>
-        <div slot="menu-actions">
+        <div slot="${SLOTS.menuActions}">
           <calcite-action text="Add">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
               <path d="M9 7h5v2H9v5H7V9H2V7h5V2h2z" />
