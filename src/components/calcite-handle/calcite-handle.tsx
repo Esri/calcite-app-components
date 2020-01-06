c4355b7d0df40d314da0c88fb230419f3e6a1b31
Harry Robbins <hrobbins@esri.com>
2020-01-06 13:38:54 -0800

    new handle component

---
+++ src/components/calcite-handle/calcite-handle.tsx
@@ -0,0 +1,89 @@
+import { Component, Element, Event, EventEmitter, Prop, h } from "@stencil/core";
+import { CSS, ICONS } from "./resources";
+
+@Component({
+  tag: "calcite-handle",
+  styleUrl: "calcite-handle.scss",
+  shadow: true
+})
+export class CalciteHandle {
+  // --------------------------------------------------------------------------
+  //
+  //  Properties
+  //
+  // --------------------------------------------------------------------------
+
+  /**
+   * @internal - stores the activated state of the drag handle.
+   */
+  @Prop({ mutable: true }) activated? = false;
+
+  // --------------------------------------------------------------------------
+  //
+  //  Private Properties
+  //
+  // --------------------------------------------------------------------------
+
+  @Element() el: HTMLCalciteHandleElement;
+
+  // --------------------------------------------------------------------------
+  //
+  //  Events
+  //
+  // --------------------------------------------------------------------------
+
+  /**
+   * Emmitted when the the handle is activated and the up or down arrow key is pressed.
+   * @event calciteHandleNudge
+   */
+  @Event() calciteHandleNudge: EventEmitter;
+
+  // --------------------------------------------------------------------------
+  //
+  //  Private Methods
+  //
+  // --------------------------------------------------------------------------
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case " ":
+        this.activated = !this.activated;
+        break;
+      case "ArrowUp":
+      case "ArrowDown":
+        if (!this.activated) {
+          return;
+        }
+        const direction = event.key.toLowerCase().replace("arrow", "");
+        this.calciteHandleNudge.emit({ handle: this.el, direction });
+        break;
+    }
+  };
+
+  handleBlur = () => {
+    this.activated = false;
+  };
+
+  handleClick = (event) => {
+    event.stopPropagation();
+  };
+
+  // --------------------------------------------------------------------------
+  //
+  //  Render Methods
+  //
+  // --------------------------------------------------------------------------
+
+  render() {
+    return (
+      <button
+        class={{ [CSS.handle]: true, [CSS.handleActivated]: this.activated }}
+        onClick={this.handleClick}
+        onKeyDown={this.handleKeyDown}
+        onBlur={this.handleBlur}
+      >
+        <calcite-icon scale="s" icon={ICONS.drag} />
+      </button>
+    );
+  }
+}
