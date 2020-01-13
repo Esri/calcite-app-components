662ea37407e7df6467dca71cdabd3b995cab9879
JC Franco <jfranco@esri.com>
2020-01-13 15:57:53 -0800

    Dedent templated markup.

--- src/components/calcite-pick-list/calcite-pick-list.stories.ts
+++ src/components/calcite-pick-list/calcite-pick-list.stories.ts
@@ -8,6 +8,7 @@ import {
 } from "../../../.storybook/utils";
 import readme from "./readme.md";
 import { ATTRIBUTES } from "../../../.storybook/resources";
+import dedent from "dedent";
 const { dir, theme } = ATTRIBUTES;
 
 export default {
@@ -53,45 +54,49 @@ const createAttributes: () => Attributes = () => [
   }
 ];
 
-const action = `<calcite-action slot="secondary-action" label="click-me" onClick="console.log('clicked');" appearance="clear">
-  <calcite-icon icon="circle" scale="s" filled style="color: #f689d8;"></calcite-icon>
-</calcite-action>`;
+const action = dedent`
+  <calcite-action slot="secondary-action" label="click-me" onClick="console.log('clicked');" appearance="clear">
+    <calcite-icon icon="circle" scale="s" filled style="color: #f689d8;"></calcite-icon>
+  </calcite-action>
+`;
 
 export const basic = () =>
   create(
     "calcite-pick-list",
     createAttributes(),
-    `
-  <calcite-pick-list-item text-heading="T. Rex" text-description="arm strength impaired" value="trex">
-    ${action}
-  </calcite-pick-list-item>
-  <calcite-pick-list-item text-heading="Triceratops" text-description="3 horn" value="triceratops" selected>
-    ${action}
-  </calcite-pick-list-item>
-  <calcite-pick-list-item text-heading="hi" text-description="there" value="helloWorld">
-    ${action}
-  </calcite-pick-list-item>`
+    dedent`
+    <calcite-pick-list-item text-heading="T. Rex" text-description="arm strength impaired" value="trex">
+      ${action}
+    </calcite-pick-list-item>
+    <calcite-pick-list-item text-heading="Triceratops" text-description="3 horn" value="triceratops" selected>
+      ${action}
+    </calcite-pick-list-item>
+    <calcite-pick-list-item text-heading="hi" text-description="there" value="helloWorld">
+      ${action}
+    </calcite-pick-list-item>
+  `
   );
 
 export const grouped = () =>
   create(
     "calcite-pick-list",
     createAttributes(),
-    `
-  <calcite-pick-list-group text-group-title="numbers">
-    <calcite-pick-list-item text-heading="one" text-description="fish" value="one" icon="grip">
-      ${action}
-    </calcite-pick-list-item>
-    <calcite-pick-list-item text-heading="two" text-description="fish" value="two" icon="grip">
-      ${action}
-    </calcite-pick-list-item>
-  </calcite-pick-list-group>
-  <calcite-pick-list-group text-group-title="colors">
-    <calcite-pick-list-item text-heading="red" text-description="fish" value="red" icon="grip">
-      ${action}
-    </calcite-pick-list-item>
-    <calcite-pick-list-item text-heading="blue" text-description="fish" value="blue" icon="grip">
-      ${action}
-    </calcite-pick-list-item>
-  </calcite-pick-list-group>`
+    dedent`
+    <calcite-pick-list-group text-group-title="numbers">
+      <calcite-pick-list-item text-heading="one" text-description="fish" value="one" icon="grip">
+        ${action}
+      </calcite-pick-list-item>
+      <calcite-pick-list-item text-heading="two" text-description="fish" value="two" icon="grip">
+        ${action}
+      </calcite-pick-list-item>
+    </calcite-pick-list-group>
+    <calcite-pick-list-group text-group-title="colors">
+      <calcite-pick-list-item text-heading="red" text-description="fish" value="red" icon="grip">
+        ${action}
+      </calcite-pick-list-item>
+      <calcite-pick-list-item text-heading="blue" text-description="fish" value="blue" icon="grip">
+        ${action}
+      </calcite-pick-list-item>
+    </calcite-pick-list-group>
+  `
   );
