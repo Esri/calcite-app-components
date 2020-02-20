88eacf2d36a3003ee54d3e032814972a8d4f31df
Matt Driscoll <MDriscoll@esri.com>
2020-02-20 10:35:25 -0800

    Enhancement: calcite-flow-item needs an onscroll event #841

--- src/components/calcite-flow-item/calcite-flow-item.tsx
+++ src/components/calcite-flow-item/calcite-flow-item.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Event, EventEmitter, Host, Prop, h } from "@stencil/core";
+import { Component, Element, Event, EventEmitter, Host, Listen, Prop, h } from "@stencil/core";
 import { VNode } from "@stencil/core/internal";
 import { focusElement, getElementDir } from "../utils/dom";
 import classnames from "classnames";
@@ -98,6 +98,12 @@ export class CalciteFlowItem {
 
   @Event() calciteFlowItemBackClick: EventEmitter;
 
+  /**
+   * Emitted when the content has been scrolled.
+   */
+
+  @Event() calciteFlowItemScroll: EventEmitter;
+
   // --------------------------------------------------------------------------
   //
   //  Private Properties
@@ -111,6 +117,12 @@ export class CalciteFlowItem {
   //  Private Methods
   //
   // --------------------------------------------------------------------------
+
+  @Listen("calcitePanelScroll")
+  handleCalcitePanelScroll(event: CustomEvent): void {
+    event.stopPropagation();
+    this.calciteFlowItemScroll.emit();
+  }
 
   queryActions(): HTMLCalciteActionElement[] {
     return Array.from(this.el.querySelectorAll(`[slot=${SLOTS.menuActions}] calcite-action`));
--- src/components/calcite-panel/calcite-panel.tsx
+++ src/components/calcite-panel/calcite-panel.tsx
@@ -102,6 +102,12 @@ export class CalcitePanel {
 
   @Event() calcitePanelDismissedChange: EventEmitter;
 
+  /**
+   * Emitted when the content has been scrolled.
+   */
+
+  @Event() calcitePanelScroll: EventEmitter;
+
   // --------------------------------------------------------------------------
   //
   //  Private Methods
@@ -116,6 +122,10 @@ export class CalcitePanel {
 
   dismiss = (): void => {
     this.dismissed = true;
+  };
+
+  panelScrollHandler = (): void => {
+    this.calcitePanelScroll.emit();
   };
 
   // --------------------------------------------------------------------------
@@ -212,7 +222,7 @@ export class CalcitePanel {
 
   renderContent(): VNode {
     return (
-      <section class={CSS.contentContainer}>
+      <section class={CSS.contentContainer} onScroll={this.panelScrollHandler}>
         <slot />
       </section>
     );
