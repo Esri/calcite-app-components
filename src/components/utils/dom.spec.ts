187598fb9ff2a4d847f6089e8e89b56d7cc6853a
JC Franco <jfranco@esri.com>
2020-03-17 11:41:42 -0700

    Issue #822: Update unit tests.

--- src/components/utils/dom.spec.ts
+++ src/components/utils/dom.spec.ts
@@ -22,8 +22,8 @@ describe("dom", () => {
     beforeEach(() => {
       document.body.innerHTML = `
       <slot-test>
-        <h2 slot=${testSlotName}>😃</h2>
-        <h2 slot=${testSlotName}>😂</h2>
+        <h2 slot=${testSlotName}><span>😃</span></h2>
+        <h2 slot=${testSlotName}><span>😂</span></h2>
       </slot-test>
     `;
     });
@@ -35,22 +35,43 @@ describe("dom", () => {
     });
 
     describe("single slotted", () => {
-      it("returns elements with matching slot name", () => {
-        expect(getSlotted(document.body, testSlotName)).toBeTruthy();
-      });
+      it("returns elements with matching slot name", () =>
+        expect(getSlotted(document.body, testSlotName)).toBeTruthy());
 
-      it("returns null when no results", () => {
-        expect(getSlotted(document.body, "non-existent-slot")).toBeNull();
+      it("returns null when no results", () => expect(getSlotted(document.body, "non-existent-slot")).toBeNull());
+
+      describe("scoped selector", () => {
+        it("returns element with matching nested selector", () =>
+          expect(getSlotted(document.body, testSlotName, { selector: "span" })).toBeTruthy());
+
+        it("returns nothing with non-matching child selector", () =>
+          expect(getSlotted(document.body, testSlotName, { selector: "non-existent-slot" })).toBeNull());
       });
     });
 
     describe("multiple slotted", () => {
-      it("returns elements with matching slot name", () => {
-        expect(getSlotted(document.body, testSlotName, true)).toHaveLength(2);
-      });
+      it("returns elements with matching slot name", () =>
+        expect(getSlotted(document.body, testSlotName, { all: true })).toHaveLength(2));
 
-      it("returns empty list when no results", () => {
-        expect(getSlotted(document.body, "non-existent-slot", true)).toHaveLength(0);
+      it("returns empty list when no results", () =>
+        expect(getSlotted(document.body, "non-existent-slot", { all: true })).toHaveLength(0));
+
+      describe("scoped selector", () => {
+        it("returns child elements matching selector", () =>
+          expect(
+            getSlotted(document.body, testSlotName, {
+              all: true,
+              selector: "span"
+            })
+          ).toHaveLength(2));
+
+        it("returns empty list with non-matching child selector", () =>
+          expect(
+            getSlotted(document.body, testSlotName, {
+              all: true,
+              selector: "non-existent"
+            })
+          ).toHaveLength(0));
       });
     });
   });
