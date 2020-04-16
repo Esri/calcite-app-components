39ac53eedfad8c91b7a91ffd673ec3dbe01b0fec
JC Franco <jfranco@esri.com>
2020-04-15 19:06:33 -0700

    chore(storybook): fix createComponentHTML storybook util
    generated ./storybook files were messing up the storybook build

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    addons: [
-        "@storybook/addon-backgrounds/register",
-        "@storybook/addon-knobs/register",
-        "@storybook/addon-notes/register-panel"
-    ],
-    presets: ["@storybook/addon-docs/preset"],
-    stories: ["../src/**/*.stories.(mdx|ts)"]
-};
