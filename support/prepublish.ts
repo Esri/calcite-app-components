ed3769d81fdca905a9718ddb2a3e3ee8d7d4fc80
JC Franco <jfranco@esri.com>
2020-04-23 17:30:17 -0700

    chore(build): revisit prepublish error messaging (#942)

--- support/prepublish.ts
+++ support/prepublish.ts
@@ -5,28 +5,14 @@ const branch = childProcess.execSync("git rev-parse --abbrev-ref HEAD");
 if (branch.toString().trim() === "master") {
   process.exit();
 } else {
-  console.log(
-    chalk.green(
-      `
-  *  ______     ______     __         ______     __     ______   ______
-  * /\\  ___\\   /\\  __ \\   /\\ \\       /\\  ___\\   /\\ \\   /\\__  _\\ /\\  ___\\
-  * \\ \\ \\____  \\ \\  __ \\  \\ \\ \\____  \\ \\ \\____  \\ \\ \\  \\/_/\\ \\/ \\ \\  __\\
-  *  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_____\\  \\ \\_____\\  \\ \\_\\    \\ \\_\\  \\ \\_____\\
-  *   \\/_____/   \\/_/\\/_/   \\/_____/   \\/_____/   \\/_/     \\/_/   \\/_____/
-  *  ______     ______     ______     ______     ______
-  * /\\  ___\\   /\\  == \\   /\\  == \\   /\\  __ \\   /\\  == \\
-  * \\ \\  __\\   \\ \\  __<   \\ \\  __<   \\ \\ \\/\\ \\  \\ \\  __<
-  *  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_____\\  \\ \\_\\ \\_\\
-  *   \\/_____/   \\/_/ /_/   \\/_/ /_/   \\/_____/   \\/_/ /_/
-  *
-  *`,
-      chalk.red(
-        `You may only run`,
-        chalk.white("$ npm publish"),
-        `from the master branch. You are on ${branch.toString()}.
-  `
-      )
-    )
+  const message = chalk.red(
+    `Error: ${chalk.white(
+      `You may only run ${chalk.green("npm publish")} from the ${chalk.yellow(
+        "master"
+      )} branch. You are on ${chalk.yellow(branch)}.`
+    )}`
   );
+
+  console.error(message);
   process.exit(1);
 }
