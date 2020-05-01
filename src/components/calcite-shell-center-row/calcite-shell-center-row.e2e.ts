f1e6283fc03da541db7847583dd482eab9069309
Alan Sangma <asangma@esri.com>
2020-05-01 13:16:19 -0700

    removed non-behavioral test.

--- src/components/calcite-shell-center-row/calcite-shell-center-row.e2e.ts
+++ src/components/calcite-shell-center-row/calcite-shell-center-row.e2e.ts
@@ -1,6 +1,3 @@
-import { newE2EPage } from "@stencil/core/testing";
-
-import { CSS } from "./resources";
 import { accessible, defaults, hidden, renders } from "../../tests/commonTests";
 
 describe("calcite-shell-center-row", () => {
@@ -23,20 +20,6 @@ describe("calcite-shell-center-row", () => {
         defaultValue: "end"
       }
     ]));
-
-  it("should show row content", async () => {
-    const page = await newE2EPage();
-
-    await page.setContent("<calcite-shell-center-row><div>content</div></calcite-shell-center-row>");
-
-    await page.waitForChanges();
-
-    const element = await page.find(`calcite-shell-center-row >>> .${CSS.content}`);
-
-    const isVisible = await element.isVisible();
-
-    expect(isVisible).toBe(true);
-  });
 
   it("should be accessible", async () =>
     accessible("<calcite-shell-center-row><div>content</div></calcite-shell-center-row>"));
