940e59170c4346674acdbc4a1797e130b50eae0c
Matt Driscoll <MDriscoll@esri.com>
2020-05-07 09:50:01 -0700

    make a11y last.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,9 @@
 module.exports = {
   addons: [
-    "@storybook/addon-a11y/register",
     "@storybook/addon-backgrounds/register",
     "@storybook/addon-knobs/register",
-    "@storybook/addon-notes/register-panel"
+    "@storybook/addon-notes/register-panel",
+    "@storybook/addon-a11y/register"
   ],
   presets: ["@storybook/addon-docs/preset"],
   stories: ["../src/**/*.stories.(mdx|ts)"]
