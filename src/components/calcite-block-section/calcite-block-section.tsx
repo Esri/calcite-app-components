25450fbf782d44542d44e376977b1b9d5b860547
Alan Sangma <asangma@esri.com>
2020-05-21 09:10:17 -0700

    Fixed RTL check. Small tweak to block-section toggle. Updated demo CSS to all for smaller containers. (#993)

--- src/components/calcite-block-section/calcite-block-section.tsx
+++ src/components/calcite-block-section/calcite-block-section.tsx
@@ -142,7 +142,7 @@ export class CalciteBlockSection {
       );
 
     return (
-      <section aria-expanded={open.toString()} class={dir ? CSS_UTILITY.rtl : null}>
+      <section aria-expanded={open.toString()} class={dir === "rtl" ? CSS_UTILITY.rtl : null}>
         {headerNode}
         <div class={CSS.content} hidden={!open}>
           <slot />
