f3e30e699092bcd23dc971f5002637360a3f64cb
Alan Sangma <asangma@esri.com>
2020-06-16 08:35:26 -0700

    Added E2E for action-bar slot. (#1007)

--- src/components/calcite-shell-center-row/calcite-shell-center-row.e2e.ts
+++ src/components/calcite-shell-center-row/calcite-shell-center-row.e2e.ts
@@ -1,3 +1,6 @@
+import { newE2EPage } from "@stencil/core/testing";
+
+import { CSS, SLOTS } from "./resources";
 import { accessible, defaults, hidden, renders } from "../../tests/commonTests";
 
 describe("calcite-shell-center-row", () => {
@@ -21,6 +24,40 @@ describe("calcite-shell-center-row", () => {
       }
     ]));
 
+  it("should not render action bar container when there is no action-bar", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent("<calcite-shell-center-row></calcite-shell-center-row>");
+
+    const actionBarContainer = await page.find(`calcite-shell-center-row >>> .${CSS.actionBarContainer}`);
+
+    expect(actionBarContainer).toBeNull();
+  });
+
+  it("should render action bar container when there is an action-bar", async () => {
+    const page = await newE2EPage();
+
+    const pageContent = `
+    <calcite-shell-center-row>
+      <calcite-action-bar slot=${SLOTS.actionBar}>
+        <calcite-action text="hello" icon="banana"></calcite-action>
+      </calcite-action-bar>
+    </calcite-shell-center-row>
+    `
+    await page.setContent(pageContent);
+
+    const actionBarContainer = await page.find(`calcite-shell-center-row >>> .${CSS.actionBarContainer}`);
+
+    expect(actionBarContainer).not.toBeNull();
+  });
+
   it("should be accessible", async () =>
-    accessible("<calcite-shell-center-row><div>content</div></calcite-shell-center-row>"));
+    accessible(`
+    <calcite-shell-center-row>
+      <div>content</div>
+      <calcite-action-bar slot=${SLOTS.actionBar}>
+        <calcite-action text="hello" icon="banana"></calcite-action>
+      </calcite-action-bar>
+    </calcite-shell-center-row>
+    `));
 });
